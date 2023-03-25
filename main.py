389bc07545b1641d51dbe5d87e3f5c10862d1095
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2023-03-25 13:08:55 +0800

    Handle failed requests

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import requests
 REQUEST_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
 }
+OUTPUT_FOLDER = "./threads"
 
 THREAD_FROM = int(sys.argv[1])
 THREAD_TO = int(sys.argv[2])
 
 
 for thread_number in range(THREAD_FROM, THREAD_TO + 1):
-    thread_output_folder = f"./threads/thread_{thread_number}"
+    thread_output_folder = os.path.join(OUTPUT_FOLDER, f"thread_{thread_number}")
     print(f"Trying to download all pages of thread {thread_number}")
     Path(thread_output_folder).mkdir(parents=True, exist_ok=True)
 
@@ -23,11 +24,15 @@ for thread_number in range(THREAD_FROM, THREAD_TO + 1):
 
         print(f"Downloading thread {thread_number} page {page_number}")
 
-        res = requests.get(
-            f"https://www.dpreview.com/forums/thread/{thread_number}?page={page_number}",
-            headers=REQUEST_HEADERS,
-        )
-        res.raise_for_status()
+        try:
+            res = requests.get(
+                f"https://www.dpreview.com/forums/thread/{thread_number}?page={page_number}",
+                headers=REQUEST_HEADERS,
+            )
+            res.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            with open(os.path.join(OUTPUT_FOLDER, f"failed.txt"), "a+") as f:
+                f.write(f"{thread_number}\t{e}\n")
 
         page_html = res.text
         with open(
